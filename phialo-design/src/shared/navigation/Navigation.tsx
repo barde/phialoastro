96e7825ac51b2ab77549545bce106c8d0a771ad7
Bartholomäus Dedersen <git@selbstapotheose.de>
2025-08-03 19:50:20 +0200

    fix: change About Me page title to About (issue #272)

--- phialo-design/src/shared/navigation/Navigation.tsx
+++ phialo-design/src/shared/navigation/Navigation.tsx
@@ -8,7 +8,7 @@ const navItems = [
   { href: '/portfolio', label: 'Portfolio' },
   { href: '/services', label: '3D für Sie', labelEn: '3D for You' },
   { href: '/classes', label: 'Classes' },
-  { href: '/about', label: 'Über mich', labelEn: 'About Me' },
+  { href: '/about', label: 'Über mich', labelEn: 'About' },
   { href: '/contact', label: 'Kontakt', labelEn: 'Contact' }
 ];
 
